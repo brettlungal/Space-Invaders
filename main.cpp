e26f47ba9ff02817fa99be4617c446b44b469973
brogrammer39 <lungalb@myumanitoba.ca>
2020-08-20 18:14:14 -0500

    beginning of logic

--- main.cpp
+++ main.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
-using namespace std
+
+using namespace std;
+
+char world[20][20] = {
+
+    "###################",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#                 #",
+    "#        W        #",
+    "###################"
+    };
+
+bool gameEnd = false;
+int gamespeed = 100;
 
 int main(void){
 
-    cout<<"Hello World!";
+    while ( !gameEnd ){
+        system("clear");
+        for ( int i = 0; i < 20; i++ ){
+            cout<<world[i]<<endl;
+        }
+
+        for ( x = 0; x < 20; x ++ ){
+            for ( y = 0; y < 20; y++ ){
+                
+                switch(world[x][y]){
+
+                    case 'W':
+                        if (GetAsyncKeyState(VK_LEFT) != 0 ){
+                            int newX = x--;
+                        }
+
+                }
+            }
+        }
+
+    }
+
     return 0;
 }
